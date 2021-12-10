dc6db22d99e545c3a2ef6e891f62667392da3043
II1CGroupe1 <64386701+II1CGroupe1@users.noreply.github.com>
2021-12-10 20:37:14 +0100

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://4b269c45.eu-gb.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
